c82f5acd2f0ed9bd6d36f86080d900d186e56c11
Stefano Mazzucco <stefano@curso.re>
2020-04-10 07:34:41 +0100

    power_widget: 0.5.3 -> 0.5.4
    Fixes spelling mistake.

--- lua/power_widget/default.nix
+++ lua/power_widget/default.nix
@@ -7,14 +7,14 @@ let
 in
 
 buildLuaPackage rec {
-  version = "0.5.3";
+  version = "0.5.4";
   name = "${simpleName}-${version}";
 
   src = fetchFromGitHub {
     owner = "stefano-m";
     repo = "awesome-${simpleName}";
     rev = "v${version}";
-    sha256 = "17gg4nd8lhmfdqw12f01v0a6c8sl12q556wsg4ysrlnlfp0rr31c";
+    sha256 = "0bnghmrh2kd623rip1fmi5sblpf7cb4g1iky7mhmv6f0825ffgkw";
   };
 
   # FIXME: cannot find awesome libraries
