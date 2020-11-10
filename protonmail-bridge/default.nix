8851d0ff7332840faeb3f5e8b074f5de8d354a67
Stefano Mazzucco <stefano@curso.re>
2020-11-10 08:25:13 +0000

    protonmail-bridge: 1.3.3 -> 1.5.0
    https://github.com/ProtonMail/proton-bridge/releases/tag/br-1.5.0
    
    - Ensured better message flow by refactoring both address and date parsing
    - Improved secure connectivity checks
    - Better deb packaging
    - More robust error handling
    
    - Ensured that conversations are properly threaded
    - Fixed Linux font issues (Fedora)
    - Better handling of Mime encrypted messages

--- protonmail-bridge/default.nix
+++ protonmail-bridge/default.nix
@@ -2,19 +2,19 @@
 
 buildGo113Module rec {
   pname = "protonmail-bridge";
-  version = "1.3.3";
-  shortRev = "01a8c9e";  # used for --version option
+  version = "1.5.0";
+  shortRev = "9a77650";  # used for --version option
 
   buildInputs = [ pkg-config libglvnd libsecret ];
 
   src = fetchFromGitHub {
     owner = "ProtonMail";
     repo = "proton-bridge";
-    rev = "v${version}";
-    sha256 = "19rpsxci9mrpviw5janjy588v4278iydlj3bsfkn99csrvn9qcl9";
+    rev = "br-${version}";
+    sha256 = "19ircj2mdb7dnp8aj17zdkbv1aq9qra1hk5jxj7dikvha4mb0yll";
   };
 
-  modSha256 = "1rb4mxv9x3byd2aag7nx14gh3sqi7f2nq0izy10ygqz1w9k3g7cb";
+  modSha256 = "13g5yd0s173mmlwjjr1pnkgji6zhp5b7hssrqn4l4j8pwb5n3h7r";
 
   subPackages = [ "cmd/Desktop-Bridge" ];
 
